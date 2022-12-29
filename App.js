341bd226112a63f2226df5d64e5eca3b1eac89ac
Sudipa Acharya <sudipaacharya351@gmail.com>
2022-12-30 01:50:51 +0530

    code added

--- App.js
+++ App.js
@@ -1,1 +1,4 @@
-
+var height = document.getElementById("height");
+var weight = document.getElementById("weight");
+var age = document.getElementById("age");
+var gender = document.getElementById("gender");
